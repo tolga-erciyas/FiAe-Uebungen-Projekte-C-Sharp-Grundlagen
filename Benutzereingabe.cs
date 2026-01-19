81a906c66d082011bec034892d428942aa00472d
tolga-erciyas <tolga.e2021@gmail.com>
2026-01-19 21:54:16 +0100

    Benutzereingabe mit Console.ReadLine umgesetzt

---
+++ Benutzereingabe.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Wie heißt du? ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hallo " + name + ", schön dich kennenzulernen!");
+    }
+}
